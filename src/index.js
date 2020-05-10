18eeb6fa619267d80bd54a73ff748ad76aa8b8fb
Gregory Pacheco <gpzim98@gmail.com>
2020-05-10 16:21:00 +0100

    Creating the first sample component

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function Test(){
+  return <h1>Hello World!</h1>
+}
+
 ReactDOM.render(
   <React.StrictMode>
+    <Test />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
