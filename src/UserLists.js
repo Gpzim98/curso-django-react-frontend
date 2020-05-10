c0896282ca3fa44459d9fb6540ad22a5ea153587
Gregory Pacheco <gpzim98@gmail.com>
2020-05-10 17:11:16 +0100

    Calling the backend api

--- src/UserLists.js
+++ src/UserLists.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ListComponent from './ListComponent';
 
-export default function UserLists(){
-    return (
-        <div>
-            <ListComponent listName={'Minha Lista'} />
-            <ListComponent listName={'Minha Lista 2'} />
-        </div>
-    )
+export default class UserLists extends React.Component{
+    state = { lists: null, loading: true }
+
+    async componentDidMount(){
+        var url = 'http://127.0.0.1:8000/list/';
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        this.setState({lists: data, loading: false});
+    }
+
+    render() 
+    {
+        return (
+            <div>
+                <ListComponent listName={'Minha Lista'} />
+                <ListComponent listName={'Minha Lista 2'} />
+            </div>
+        )
+    }
 }
