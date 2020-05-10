40722b311eb9fe1f43fcfe0b0c6cf020030fe1d5
Gregory Pacheco <gpzim98@gmail.com>
2020-05-10 21:18:27 +0100

    Reading the token from api and saving it to the local storage

--- src/LoginComponent.js
+++ src/LoginComponent.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class LoginComponent extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {username: '', password: ''};
+        this.state = {username: 'Test', password: 'CursoDjango'};
     
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -19,7 +19,15 @@ export default class LoginComponent extends React.Component{
       }
     
       handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.username + ' Password: ' + this.state.password );
+        var url = 'http://127.0.0.1:8000/api-token-auth/';
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username: this.state.username, password: this.state.password })
+        };
+        fetch(url, requestOptions)
+            .then(response => response.json())
+            .then(data => localStorage.setItem('token', data.token));
         event.preventDefault();
       }
     
--- src/UserLists.js
+++ src/UserLists.js
@@ -11,7 +11,7 @@ export default class UserLists extends React.Component{
                 'Content-Type': 'application/json'
             }
         }
-        config.headers['Authorization'] = 'Token ac75a7724117da0ec6c5fae3052887c03ae3e2e2';
+        config.headers['Authorization'] = 'Token ' + localStorage.getItem('token');
 
         var url = 'http://127.0.0.1:8000/list/';
         const response = await fetch(url, config);
@@ -23,9 +23,9 @@ export default class UserLists extends React.Component{
     render() 
     {
         const listsApi = this.state.lists;
-        var token = '';
+        var token = localStorage.getItem('token');
 
-        if(token==='')
+        if(!token)
         {
             return <LoginComponent />
         }
