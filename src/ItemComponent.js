0937980170d9615214f9b118ffc366403c1fc27b
Gregory Pacheco <gpzim98@gmail.com>
2020-06-07 17:25:46 +0100

    Adding template HTML5

--- src/ItemComponent.js
+++ src/ItemComponent.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default function ItemComponent(props){
     const status = props.status;
     return <li>{ props.name } 
-        <div>Status: {status ? <div>Finalizado</div> : <div>Nao Finalizado</div>} </div>
+        Status: {status ? <span>Finalizado</span> : <span>Nao Finalizado</span>}
     </li>
 }
