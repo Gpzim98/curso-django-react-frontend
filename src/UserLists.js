6c73831da93145e9399c3b1a8b5b78275e59d965
Gregory Pacheco <gpzim98@gmail.com>
2020-06-07 18:48:43 +0100

    Setting the right address to the API

--- src/UserLists.js
+++ src/UserLists.js
@@ -12,7 +12,7 @@ export default class UserLists extends React.Component{
         }
         config.headers['Authorization'] = 'Token ' + localStorage.getItem('token');
 
-        var url = 'http://127.0.0.1:8000/list/';
+        var url = 'http://51.143.150.114:8080/list/';
         const response = await fetch(url, config);
         const data = await response.json();
         console.log(data);
